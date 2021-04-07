190d66507658c4747cc524fe8b559de9ee64b7d3
Fatih Durmaz <fatihdurmaz.93@gmail.com>
2021-04-07 16:51:06 +0300

    First comment

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Fatih Durmaz on 7.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hi")
+    }
+
+
+}
+
